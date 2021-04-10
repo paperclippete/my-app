dfec45308b5dc708ceab6f99bb21bdf6903c5034
Alexa Gibson <alexa.gibson@sky.uk>
2021-04-10 16:37:37 +0100

    adding tic tac toe game in functional components

---
+++ src/components/square.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const Square = ({ value, onClick}) => {
+
+  return (
+    <button 
+      className="square" 
+      onClick={onClick}
+    >
+      {value}
+    </button>
+  );
+}
